4aba86d15f13be7cb9b46c5934a6aeeac9d927f1
kennedybryant <kennedy_marisa96@yahoo.com>
2021-07-21 16:45:17 -0400

    All Tasks Complete

--- index.js
+++ index.js
@@ -39,15 +39,39 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person(/*code here*/) {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat= function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name} ${this.age}`;
+}
 
+const kevin = new Person('Kevin', 26);
+const leo = new Person('Leo', 25);
 
+kevin.eat('🍕');
+kevin.eat('🌮');
+kevin.eat('🍱');
+kevin.eat('🍝');
+kevin.eat('🍔');
 
+console.log(kevin.stomach);
+kevin.poop();
+console.log(kevin.stomach);
+console.log(kevin.toString());
 
 /*
   TASK 2
@@ -63,8 +87,15 @@ function Person(/*code here*/) {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
 }
 
 
@@ -75,18 +106,27 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+
 }
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
+
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Object Binding - if this other principles do not apply, "this" refers to the window, the global object in node, or will come back as undefined in strict mode.
+  2. Implicit Binding - whenever there is a dot in front of the function when it's invoked, the object before the dot is what "this" refers to.
+  3. New Binding - uses the "new" keyword to construct a new object. "this" refers to the new object
+  4. Explicit Binding - "this" is defined when we use .call, .apply, or .bind method
 */
 
 
